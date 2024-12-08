0df481f92bfb52e9abe0239f8c6deda5fc4951a6
higaki koji <pakkun_kh@fol.hi-ho.ne.jp>
2024-12-08 21:13:12 +0900

    fixed test code

--- test/test.c
+++ test/test.c
@@ -5,20 +5,37 @@
 int main(int argc, char *argv[]) {
   const char *path = "C:\\users\\pakkun\\data";
   const char *extension = "csv";
-  int ret = p_get_file_count(path, extension, 1);
-  p_printf(NULL, "%d\n", ret);
+  int count = p_get_file_count(path, extension, 1);
+  if (count < 0) {
+    return -1;
+  }
+  p_printf(NULL, "%d\n", count);
 
-  char **files = (char **)malloc(sizeof(char *) * ret);
-  for (int i = 0; i < ret; i++) {
-    files[i] = (char *)malloc(sizeof(char) * 154);
+  char **files = (char **)malloc(sizeof(char *) * count);
+  if (files == NULL) {
+    return -1;
   }
-  ret = p_get_files(path, extension, 1, files, 154);
-  for (int i = 0; i < ret; i++) {
-    p_printf(NULL, "%s\n", files[i]);
-  }
-  for (int i = 0; i < ret; i++) {
+
+  int ret = 0;
+  int alocated_count = 0;
+  do {
+    for (alocated_count = 0; alocated_count < count; alocated_count++) {
+      files[alocated_count] = (char *)malloc(sizeof(char) * _MAX_PATH);
+      if (files[alocated_count] == NULL) {
+        ret = -1;
+        break;
+      }
+    }
+    count = p_get_files(path, extension, 1, files, _MAX_PATH);
+    for (int i = 0; i < alocated_count; i++) {
+      p_printf(NULL, "%s\n", files[i]);
+    }
+  } while (0);
+
+  for (int i = 0; i < alocated_count; i++) {
     free(files[i]);
   }
   free(files);
-  return 0;
+
+  return ret;
 }
